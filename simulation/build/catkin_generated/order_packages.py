103b33673f7a2a091bcdc183729c98200fae6417
Muhammet Ali Şentürk <88962496+MAli7319@users.noreply.github.com>
2024-05-11 13:17:05 +0300

    Delete simulation directory

--- simulation/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/moborobot/Moborobo-Project/simulation/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/moborobot/Moborobo-Project/on_robot/devel;/home/moborobot/RoboVoyager/devel;/opt/ros/noetic'.split(';') if '/home/moborobot/Moborobo-Project/on_robot/devel;/home/moborobot/RoboVoyager/devel;/opt/ros/noetic' != '' else []
--- simulation/build/robosense_simulator/robosense_simulator/robosense_simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moborobot/Moborobo-Project/simulation/src/robosense_simulator/robosense_simulator/robosense_simulator
-# Build directory: /home/moborobot/Moborobo-Project/simulation/build/robosense_simulator/robosense_simulator/robosense_simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
